8d5c3cc9800b185d22d393482b65ae4bd185a1c4
Josh Sixsmith <josh.sixsmith@gmail.com>
2020-03-04 14:25:41 +1100

    Updated to specify the output log files.

--- submit-C2.bash
+++ submit-C2.bash
@@ -12,4 +12,4 @@ module use /g/data/v10/public/modules/modulefiles
 module load wagl/5.4.1
 module load openmpi
 
-mpiexec -n 240 python workflow.py --reference-dir /g/data/rs0/scenes/nbar-scenes-tmp/ls8/2019/12/output --test-dir /g/data/rs0/scenes/nbar-scenes-tmp/ls8/2019/12/output/nbar --out-pathname /g/data/v10/testing_ground/jps547/gadi-test/C2/diff-results-test/c2-raijin-gadi-nbar-comparison.h5 --pattern 'ga-metadata.yaml'
+mpiexec -n 240 python workflow.py --reference-dir /g/data/rs0/scenes/nbar-scenes-tmp/ls8/2019/12/output --test-dir /g/data/rs0/scenes/nbar-scenes-tmp/ls8/2019/12/output/nbar --out-pathname /g/data/v10/testing_ground/jps547/gadi-test/C2/diff-results-test/c2-raijin-gadi-nbar-comparison.h5 --pattern 'ga-metadata.yaml' --log-pathname status-c2.log

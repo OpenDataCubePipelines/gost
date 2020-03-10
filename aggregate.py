918073e2064fcf35b55d9d458d0288d2df779bea
Josh Sixsmith <josh.sixsmith@gmail.com>
2020-03-10 21:56:20 +1100

    Updated to simplify the function call for categorical data, as well as for the output csv file.

--- aggregate.py
+++ aggregate.py
@@ -14,142 +14,63 @@ import pandas
 import geopandas
 
 
-def aggregate_general_results(pathname, outdir):
-    """Aggregate the general results table"""
+FUNCS = [numpy.min, numpy.max, numpy.mean]
+
+
+def summarise_general_results(pathname, out_pathname):
+    """Summarise the general results table"""
     gdf = geopandas.read_file(str(pathname))
 
-    out_pathname = outdir.joinpath('min-aggregate-general-results-minmax.csv')
-    pivot = pandas.pivot_table(gdf, index=['measurement'],
-                               values=['minv', 'maxv'], aggfunc=numpy.min)
-    pivot.to_csv(str(out_pathname))
+    cols = ['minv', 'maxv', 'percent_different']
 
-    out_pathname = outdir.joinpath('max-aggregate-general-results-minmax.csv')
-    pivot = pandas.pivot_table(gdf, index=['measurement'],
-                               values=['minv', 'maxv'], aggfunc=numpy.max)
-    pivot.to_csv(str(out_pathname))
+    # out_pathname = outdir.joinpath('summary-general-results.csv')
+    pivot = pandas.pivot_table(gdf, index=['measurement'], values=cols,
+                               aggfunc=FUNCS)
 
-    fname = 'max-aggregate-general-results-percent-different.csv'
-    out_pathname = outdir.joinpath(fname)
-    pivot = pandas.pivot_table(gdf, index=['measurement'],
-                               values=['percent_different'], aggfunc=numpy.max)
+    # we're reshaping here simply to get a cleaner table structure to output
+    pivot = pivot.transpose().unstack().transpose()
+
     pivot.to_csv(str(out_pathname))
 
 
-def aggregate_fmask_results(pathname, outdir):
-    """Aggregate the fmask results table"""
+def summarise_catgegorical_results(pathname, out_pathname):
+    """Summarise a generic categorical table"""
     gdf = geopandas.read_file(str(pathname))
 
-    null = [i for i in gdf.columns if 'null_2' in i]
-    clear = [i for i in gdf.columns if 'clear_2' in i]
-    cloud = [i for i in gdf.columns if 'cloud_2' in i]
-    shadow = [i for i in gdf.columns if 'shadow_2' in i]
-    snow = [i for i in gdf.columns if 'snow_2' in i]
-    water = [i for i in gdf.columns if 'water_2' in i]
+    # we are assuming columns of interest are of the format <field_2_field>
+    cols = [i for i in gdf.columns if '_2_' in i]
 
-    out_pathname = outdir.joinpath('max-aggregate-fmask-results-null.csv')
-    pivot = pandas.pivot_table(gdf, index=['measurement'], values=null,
-                               aggfunc=numpy.max)
-    pivot.to_csv(str(out_pathname))
+    pivot = pandas.pivot_table(gdf, index=['measurement'], values=cols,
+                               aggfunc=FUNCS)
 
-    out_pathname = outdir.joinpath('max-aggregate-fmask-results-clear.csv')
-    pivot = pandas.pivot_table(gdf, index=['measurement'], values=clear,
-                               aggfunc=numpy.max)
-    pivot.to_csv(str(out_pathname))
+    # we're reshaping here simply to get a cleaner table structure to output
+    pivot = pivot.transpose().unstack().transpose()
 
-    out_pathname = outdir.joinpath('max-aggregate-fmask-results-cloud.csv')
-    pivot = pandas.pivot_table(gdf, index=['measurement'], values=cloud,
-                               aggfunc=numpy.max)
-    pivot.to_csv(str(out_pathname))
-
-    out_pathname = outdir.joinpath('max-aggregate-fmask-results-shadow.csv')
-    pivot = pandas.pivot_table(gdf, index=['measurement'], values=shadow,
-                               aggfunc=numpy.max)
-    pivot.to_csv(str(out_pathname))
-
-    out_pathname = outdir.joinpath('max-aggregate-fmask-results-snow.csv')
-    pivot = pandas.pivot_table(gdf, index=['measurement'], values=snow,
-                               aggfunc=numpy.max)
-    pivot.to_csv(str(out_pathname))
-
-    out_pathname = outdir.joinpath('max-aggregate-fmask-results-water.csv')
-    pivot = pandas.pivot_table(gdf, index=['measurement'], values=water,
-                               aggfunc=numpy.max)
-    pivot.to_csv(str(out_pathname))
-
-
-def aggregate_contiguity_results(pathname, outdir):
-    """Aggregate the contiguity results table"""
-    gdf = geopandas.read_file(str(pathname))
-
-    non_contiguous = [i for i in gdf.columns if 'non_contiguous_2' in i]
-    contiguous = [i for i in gdf.columns if 'contiguous_2' in i]
-
-    fname = 'max-aggregate-contiguity-results-non-contiguous.csv'
-    out_pathname = outdir.joinpath(fname)
-    pivot = pandas.pivot_table(gdf, index=['measurement'],
-                               values=non_contiguous,
-                               aggfunc=numpy.max)
-    pivot.to_csv(str(out_pathname))
-
-    fname = 'max-aggregate-contiguity-results-contiguity.csv'
-    out_pathname = outdir.joinpath(fname)
-    pivot = pandas.pivot_table(gdf, index=['measurement'], values=contiguous,
-                               aggfunc=numpy.max)
-    pivot.to_csv(str(out_pathname))
-
-
-def aggregate_shadow_results(pathname, outdir):
-    """Aggregate the terrain shadow results table"""
-    gdf = geopandas.read_file(str(pathname))
-
-    shadow = [i for i in gdf.columns if 'shaded_2' in i]
-    unshaded = [i for i in gdf.columns if 'unshaded_2' in i]
-
-    fname = 'max-aggregate-terrain-shadow-results-shadow.csv'
-    out_pathname = outdir.joinpath(fname)
-    pivot = pandas.pivot_table(gdf, index=['measurement'], values=shadow,
-                               aggfunc=numpy.max)
-    pivot.to_csv(str(out_pathname))
-
-    fname = 'max-aggregate-terrain-shadow-results-unshaded.csv'
-    out_pathname = outdir.joinpath(fname)
-    pivot = pandas.pivot_table(gdf, index=['measurement'], values=unshaded,
-                               aggfunc=numpy.max)
     pivot.to_csv(str(out_pathname))
 
 
 @click.command()
-@click.option("--pathname1",
+@click.option("--pathname",
               help="The pathname for the generak comparison input file.",
               type=click.Path(file_okay=True, dir_okay=False))
-@click.option("--pathname2",
-              help="The pathname for the fmask comparison input file.",
+@click.option("--out-pathname",
+              help="The pathname for the summary output file.",
               type=click.Path(file_okay=True, dir_okay=False))
-@click.option("--pathname3",
-              help="The pathname for the contiguity comparison input file.",
-              type=click.Path(file_okay=True, dir_okay=False))
-@click.option("--pathname4",
-              help="The pathname for the shadow comparison input file.",
-              type=click.Path(file_okay=True, dir_okay=False))
-@click.option("--outdir", type=click.Path(file_okay=False, writable=True),
-              help=("The base output directory to contain the summarised "
-                    "statistics."))
-def main(pathname1, pathname2, pathname3, pathname4, outdir):
+@click.option("--categorical", default=False, is_flag=True,
+              help="If set, the input file should contain categorical data")
+def main(pathname, out_pathname, categorical):
     """Produce summary statistics for the wagl and Fmask outputs."""
-    pathname1 = Path(pathname1)
-    pathname2 = Path(pathname2)
-    pathname3 = Path(pathname3)
-    pathname4 = Path(pathname4)
-    outdir = Path(outdir)
+    pathname = Path(pathname)
+    out_pathname = Path(out_pathname)
 
-    if not outdir.exists():
-        outdir.mkdir()
+    if not out_pathname.parent.exists():
+        out_pathname.parent.mkdir()
 
-    # aggregate each component
-    aggregate_general_results(pathname1, outdir)
-    aggregate_fmask_results(pathname2, outdir)
-    aggregate_contiguity_results(pathname3, outdir)
-    aggregate_shadow_results(pathname4, outdir)
+    # summarise the table
+    if categorical:
+        summarise_catgegorical_results(pathname, out_pathname)
+    else:
+        summarise_general_results(pathname, out_pathname)
 
 
 if __name__ == '__main__':

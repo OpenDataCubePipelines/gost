c06b21b666709ad6ce03373bbf8226111137ca74
Josh Sixsmith <josh.sixsmith@gmail.com>
2020-08-26 13:19:52 +1000

    Initial upload of setup script.

---
+++ setup.py
@@ -0,0 +1,28 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="gost",
+    version="0.0.1",
+    url="https://github.com/sixy6e/gost",
+    author="Josh Sixsmith",
+    description="Structured logging for MPI based Python code",
+    keywords=[
+        "logging", 
+        "structured",
+        "structure",
+        "log",
+        "MPI",
+        "mpi4py",
+    ],
+    packages=find_packages(),
+    install_requires=[
+        "mpi4py",
+        "click",
+        "structlog",
+    ],
+    license="MIT",
+    entry_points="""
+        [console_scripts]
+        ard-intercomparison=gost.ui.cli:entry_point
+    """,
+)

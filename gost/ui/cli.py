630b8df77c139d2e953b7cfd020d03c778ea5e0b
Josh Sixsmith <josh.sixsmith@gmail.com>
2020-08-26 14:25:31 +1000

    Updated command list.

--- gost/ui/cli.py
+++ gost/ui/cli.py
@@ -1,9 +1,7 @@
 import click
 
-# from gost.commands import comparison, merge, pbs, plotting, query, summarise
 from .commands import comparison, merge, pbs, plotting, query, summarise
-# from .. import scripts
-# from gost.scripts import comparison, merge, pbs, plotting, query, summarise
+
 
 @click.group()
 def entry_point():
@@ -13,10 +11,9 @@ def entry_point():
     pass
 
 
-entry_point.add_command(comparison.comparison)
-entry_point.add_command(merge.merge)
 entry_point.add_command(pbs.pbs)
-entry_point.add_command(plotting.plotting)
 entry_point.add_command(query.query)
 entry_point.add_command(query.query_filesystem)
-entry_point.add_command(summarise.summarise)
+entry_point.add_command(comparison.comparison)
+entry_point.add_command(query.collate)
+entry_point.add_command(plotting.plotting)

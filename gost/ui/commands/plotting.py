e03a797f357844610f7b62312078c5a61b5593df
Josh Sixsmith <josh.sixsmith@gmail.com>
2020-08-31 10:23:14 +1000

    Added substance to the plotting command util.

--- gost/ui/commands/plotting.py
+++ gost/ui/commands/plotting.py
@@ -1,6 +1,21 @@
+from pathlib import Path
 import click
+import structlog
+import geopandas
 
+from gost.constants import (
+    DatasetNames,
+    DirectoryNames,
+    FileNames,
+    LOG_PROCESSORS,
+    LogNames,
+    MergeLookup,
+    SummaryLookup,
+)
+from gost.plot_utils import plot_png
 from ._shared_commands import io_dir_options, db_query_options
+
+_LOG = structlog.get_logger()
 
 
 @click.command()
@@ -11,3 +26,21 @@ def plotting(
     """
     Using the framing geometry, plot the results of the intercomparison evaluation.
     """
+
+    outdir = Path(outdir)
+    log_fname = outdir.joinpath(DirectoryNames.LOGS.value, LogNames.PLOTTING.value)
+
+    if not log_fname.parent.exists():
+        log_fname.parent.mkdir(parents=True)
+
+    with open(log_fname, "w") as fobj:
+        structlog.configure(logger_factory=structlog.PrintLoggerFactory(fobj))
+
+        results_fname = outdir.joinpath(
+            DirectoryNames.RESULTS.value, FileNames.GENERAL_FRAMING.value
+        )
+
+        _LOG.info("opening geometry framing results file", fname=str(results_fname))
+        gdf = geopandas.read_file(results_fname)
+
+        plot_png(gdf, outdir)

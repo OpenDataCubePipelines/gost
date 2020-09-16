0d9488a64b20c4385c308db2efa81ffbf2b8efaf
Josh Sixsmith <josh.sixsmith@gmail.com>
2020-09-17 09:45:16 +1000

    Updated the _write_product_docs to incorporate a pass or fail string in the title, as well as to incorporate the generation of tables.

--- gost/report_utils.py
+++ gost/report_utils.py
@@ -3,23 +3,33 @@ Utils pertaining to the creation of the LaTeX reports.
 """
 import io
 from pathlib import Path
-from typing import Dict, List
+from typing import Any, Dict, List, Tuple
+import pandas
 import geopandas  # type: ignore
 import structlog  # type: ignore
-import zstandard  # type: ignore
 
+from gost.collate import reflectance_pass_fail
 from gost.constants import (
     DirectoryNames,
     FileNames,
     MEASUREMENT_TEMPLATE,
     DOCUMENT_TEMPLATE,
+    TABLE1_TEMPLATE,
 )
+
+MIN_MAX_PCT_CAPTION = {
+    "nbar": "difference in surface reflectance (units are \% reflectance)",
+    "nbart": "difference in surface reflectance (units are \% reflectance)",
+    "oa": "difference",
+}
 
 _LOG = structlog.get_logger()
 
 
 def _write_measurement_docs(
-    gdf: geopandas.GeoDataFrame, outdir: Path, measurement_template: str
+    gdf: geopandas.GeoDataFrame,
+    outdir: Path,
+    measurement_template: str,
 ) -> Dict[str, List]:
     """
     Write the measurement sub-documents for each of the product groups.
@@ -61,18 +71,27 @@ def _write_measurement_docs(
 
 
 def _write_product_docs(
-    measurement_doc_fnames: Dict[str, List], outdir: Path, document_template: str
+    measurement_doc_fnames: Dict[str, List],
+    table_fnames: Dict[str, str],
+    pass_fail: Dict[str, str],
+    outdir: Path,
+    document_template: str,
 ) -> None:
     """Write each of the LaTeX main level product documents."""
 
     for product_group in measurement_doc_fnames:
 
-        sub_doc_names = measurement_doc_fnames[product_group]
+        sub_doc_names = measurement_doc_fnames[product_group].copy()
+
+        # only have 1 table doc to insert at this stage
+        sub_doc_names.insert(0, table_fnames[product_group])
 
         doc_sections = "".join([f"  \\subfile{{{s}}}\n" for s in sub_doc_names])
 
         out_string = document_template.format(
-            product_group=product_group, sections=doc_sections
+            product_group=product_group,
+            sections=doc_sections,
+            pass_fail=pass_fail[product_group],
         )
 
         out_fname = outdir.joinpath(
@@ -85,7 +104,38 @@ def _write_product_docs(
             outf.write(out_string)
 
 
-def latex_documents(gdf: geopandas.GeoDataFrame, outdir: Path) -> None:
+def _write_product_tables(outdir: Path, table_template: str) -> Dict[str, str]:
+    """Create LaTeX tables from various CSV's."""
+
+    product_groups = ["nbar", "nbart", "oa"]
+
+    table_fnames: Dict[str, str] = dict()
+
+    for product_name in product_groups:
+
+        basename = f"{product_name}_tables.tex"
+        relative_fname = Path(DirectoryNames.TABLE_DOCS.value, basename)
+        table_fnames[product_name] = str(relative_fname)
+
+        out_fname = outdir.joinpath(relative_fname)
+        if not out_fname.parent.exists():
+            out_fname.parent.mkdir(parents=True)
+
+        out_string = table_template.format(
+            caption=MIN_MAX_PCT_CAPTION[product_name], product_name=product_name
+        )
+
+        _LOG.info("writing tex document to disk", out_fname=str(out_fname))
+
+        with open(out_fname, "w") as src:
+            src.write(out_string)
+
+    return table_fnames
+
+
+def latex_documents(
+    gdf: geopandas.GeoDataFrame, dataframe: pandas.DataFrame, outdir: Path
+) -> None:
     """
     Utility to create the latex document strings.
     Very basic, but is a starting point for auto-generated nice looking
@@ -100,22 +150,35 @@ def latex_documents(gdf: geopandas.GeoDataFrame, outdir: Path) -> None:
 
     _LOG.info("reading LaTeX main document template")
 
-    with open(DOCUMENT_TEMPLATE, "rb") as src:
-        dctx = zstandard.ZstdDecompressor()
-        doc_template = io.TextIOWrapper(
-            dctx.stream_reader(src), encoding="utf-8"
-        ).read()
+    with open(DOCUMENT_TEMPLATE, "r") as src:
+        doc_template = src.read()
 
     _LOG.info("reading LaTeX measurement template")
 
-    with open(MEASUREMENT_TEMPLATE, "rb") as src:
-        dctx = zstandard.ZstdDecompressor()
-        measurement_template = io.TextIOWrapper(
-            dctx.stream_reader(src), encoding="utf-8"
-        ).read()
+    with open(MEASUREMENT_TEMPLATE, "r") as src:
+        measurement_template = src.read()
+
+    _LOG.info("reading LaTeX table template")
+
+    with open(TABLE1_TEMPLATE, "r") as src:
+        table_template = src.read()
 
     measurement_doc_fnames = _write_measurement_docs(gdf, outdir, measurement_template)
 
-    _write_product_docs(measurement_doc_fnames, outdir, doc_template)
+    table_fnames = _write_product_tables(outdir, table_template)
+
+    pass_fail_result = reflectance_pass_fail(dataframe)
+    nbar_pass_fail = "Pass" if pass_fail_result[0]["test_passed"] else "Fail"
+    nbart_pass_fail = "Pass" if pass_fail_result[1]["test_passed"] else "Fail"
+
+    pass_fail = {
+        "nbar": f"Pass or Fail: {nbar_pass_fail}",
+        "nbart": f"Pass or Fail: {nbart_pass_fail}",
+        "oa": "",
+    }
+
+    _write_product_docs(
+        measurement_doc_fnames, table_fnames, pass_fail, outdir, doc_template
+    )
 
     _LOG.info("finished writing LaTeX documents")

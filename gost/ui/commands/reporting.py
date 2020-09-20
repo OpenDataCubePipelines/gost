3187462d3448700020fde545739925bb3326ce03
Josh Sixsmith <josh.sixsmith@gmail.com>
2020-09-20 22:34:39 +1000

    Partioned out the proc info csv creation into its own function.

--- gost/ui/commands/reporting.py
+++ gost/ui/commands/reporting.py
@@ -5,11 +5,13 @@ from pathlib import Path, PurePosixPath as PPath
 from typing import Union
 import click
 import h5py  # type: ignore
+import pandas
 import structlog  # type: ignore
 import geopandas  # type: ignore
 
 from wagl.hdf5 import read_h5_table  # type: ignore
 from gost.constants import (
+    CsvFileNames,
     DatasetGroups,
     DatasetNames,
     DirectoryNames,
@@ -22,6 +24,46 @@ from gost.report_utils import latex_documents
 from ._shared_commands import io_dir_options
 
 _LOG = structlog.get_logger()
+
+
+def _extract_proc_info_results(fid: h5py.File, outdir: Path) -> None:
+    """
+    Extracts the ancillary, gqa, software versions results tables
+    and converts to CSV for the LaTeX document.
+    """
+
+    def _read_table(fid: h5py.File, dataset_name: PPath) -> pandas.DataFrame:
+        """Small proxy to read the H5 table dataset."""
+
+        _LOG.info("reading dataset", dataset_name=str(dataset_name))
+        dataframe = read_h5_table(fid, str(dataset_name))
+
+        return dataframe
+
+    dataset_name = PPath(
+        DatasetGroups.SUMMARY.value, DatasetNames.ANCILLARY_SUMMARISED.value
+    )
+    ancillary_df = _read_table(fid, dataset_name)
+
+    dataset_name = PPath(DatasetGroups.SUMMARY.value, DatasetNames.GQA_SUMMARISED.value)
+    gqa_df = _read_table(fid, dataset_name)
+
+    dataset_name = PPath(DatasetNames.SOFTWARE_VERSIONS.value)
+    software_df = _read_table(fid, dataset_name)
+
+    out_fname = outdir.joinpath(
+        DirectoryNames.RESULTS.value, CsvFileNames.ANCILLARY.value
+    )
+    create_csv(ancillary_df, out_fname)
+
+    out_fname = outdir.joinpath(DirectoryNames.RESULTS.value, CsvFileNames.GQA.value)
+    create_csv(gqa_df, out_fname)
+
+    out_fname = outdir.joinpath(
+        DirectoryNames.RESULTS.value, CsvFileNames.SOFTWARE.value
+    )
+    _LOG.info("writing CSV", out_fname=str(out_fname))
+    software_df.to_csv(out_fname, index=False)
 
 
 @click.command()
@@ -60,39 +102,11 @@ def reporting(
             _LOG.info("reading dataset", dataset_name=str(dataset_name))
             dataframe = read_h5_table(fid, str(dataset_name))
 
-            # read intercomparison ancillary summary
-            dataset_name = PPath(
-                DatasetGroups.SUMMARY.value, DatasetNames.ANCILLARY_SUMMARISED.value
-            )
-            _LOG.info("reading dataset", dataset_name=str(dataset_name))
-            ancillary_df = read_h5_table(fid, str(dataset_name))
-
-            # read intercomparison gqa summary
-            dataset_name = PPath(
-                DatasetGroups.SUMMARY.value, DatasetNames.GQA_SUMMARISED.value
-            )
-            _LOG.info("reading dataset", dataset_name=str(dataset_name))
-            gqa_df = read_h5_table(fid, str(dataset_name))
-
-            dataset_name = PPath(DatasetNames.SOFTWARE_VERSIONS.value)
-            _LOG.info("reading dataset", dataset_name=str(dataset_name))
-            software_df = read_h5_table(fid, str(dataset_name))
-
+            # read and convert metadata tables
+            _extract_proc_info_results(fid, outdir)
 
         _LOG.info("creating CSV's of the general measurements intercomparison summary")
         create_general_csvs(dataframe, outdir.joinpath(DirectoryNames.RESULTS.value))
-
-        _LOG.info("creating CSV of the ancillary intercomparison summary")
-        out_fname = outdir.joinpath(DirectoryNames.RESULTS.value, "ancillary_min_max.csv")
-        create_csv(ancillary_df, out_fname)
-
-        _LOG.info("creating CSV of the gqa intercomparison summary")
-        out_fname = outdir.joinpath(DirectoryNames.RESULTS.value, "gqa_min_max.csv")
-        create_csv(gqa_df, out_fname)
-
-        _LOG.info("creating CSV of the software versions")
-        out_fname = outdir.joinpath(DirectoryNames.RESULTS.value, "software-versions.csv")
-        software_df.to_csv(out_fname, index=False)
 
         results_fname = outdir.joinpath(
             DirectoryNames.RESULTS.value, FileNames.GENERAL_FRAMING.value

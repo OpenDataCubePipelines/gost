926550a2e8ce21e531a3d89956c048bbb61c84e5
Josh Sixsmith <josh.sixsmith@gmail.com>
2021-01-22 14:17:52 +1100

    Initial upload of test suite and data files.

---
+++ tests/__init__.py
@@ -0,0 +1,20 @@
+from pathlib import Path
+
+LS5_ODC_DOC_PATH = Path(__file__).parent.joinpath(
+    "data", "ga_ls5t_ard_3-0-0_092084_2008-09-25_final.odc-metadata.yaml"
+)
+LS7_ODC_DOC_PATH = Path(__file__).parent.joinpath(
+    "data", "ga_ls7e_ard_3-1-0_092080_2020-12-07_final.odc-metadata.yaml"
+)
+LS8_ODC_DOC_PATH = Path(__file__).parent.joinpath(
+    "data", "ga_ls8c_ard_3-1-0_090083_2020-07-26_final.odc-metadata.yaml"
+)
+LS5_ODC_PROC_PATH = Path(__file__).parent.joinpath(
+    "data", "ga_ls5t_ard_3-0-0_092084_2008-09-25_final.proc-info.yaml"
+)
+LS7_ODC_PROC_PATH = Path(__file__).parent.joinpath(
+    "data", "ga_ls7e_ard_3-1-0_092080_2020-12-07_final.proc-info.yaml"
+)
+LS8_ODC_PROC_PATH = Path(__file__).parent.joinpath(
+    "data", "ga_ls8c_ard_3-1-0_090083_2020-07-26_final.proc-info.yaml"
+)
